afe42f5c4613d45432903f793051d05f4b86da32
S.Chakradhar Reddy <chakradharreddysangati@gmail.com>
2026-02-03 00:08:52 +0530

    Updated mining

--- src/mining.py
+++ src/mining.py
@@ -3,15 +3,14 @@ from block import Block
 def mine_block(miner, mempool, utxo_manager, num_txs=5):
     selected = mempool.get_top_transactions(num_txs, utxo_manager)
     total_fees = 0.0
-
+    for tx in selected:
+        total_fees += tx.fee(utxo_manager)
     for tx in selected:
         for tx_id, index in tx.inputs:
             utxo_manager.remove_utxo(tx_id, index)
 
         for i, (amount, owner) in enumerate(tx.outputs):
             utxo_manager.add_utxo(tx.tx_id, i, amount, owner)
-
-        total_fees += tx.fee(utxo_manager)
 
     # Coinbase reward
     utxo_manager.add_utxo("coinbase", 0, total_fees, miner)
