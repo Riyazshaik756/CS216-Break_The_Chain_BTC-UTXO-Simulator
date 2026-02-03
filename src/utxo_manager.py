544b020eccbfd4f0877adae19f4624cc64312612
Kunal1951 <kanna1951693@gmail.com>
2026-02-03 12:26:08 +0530

    Uddated Utxo_maneger

--- src/utxo_manager.py
+++ src/utxo_manager.py
@@ -14,18 +14,16 @@ class UTXOManager:
             del self.utxo_set[(tx_id, index)]
 
     def get_balance(self, owner: str) -> float:
-        total = 0.0
-        for key in self.utxo_set:
-            if self.utxo_set[key][1] == owner:
-                total += self.utxo_set[key][0]
-        return total
+        return sum(
+            amount
+            for (amount, utxo_owner) in self.utxo_set.values()
+            if utxo_owner == owner
+    )
 
     def exists(self, tx_id: str, index: int) -> bool:
         return (tx_id, index) in self.utxo_set
 
-    def get_utxos_for_owner(self, owner: str) -> list:
-        utxo_list = []
-        for (tx_id, index), (amount, utxo_owner) in self.utxo_set.items():
-            if utxo_owner == owner:
-                utxo_list.append((tx_id, index, amount))
-        return utxo_list
+    def get_utxo(self, tx_id: str, index: int):
+        if (tx_id, index) not in self.utxo_set:
+            raise ValueError(f"UTXO not found: tx_id={tx_id}, index={index}")
+        return self.utxo_set[(tx_id, index)]
